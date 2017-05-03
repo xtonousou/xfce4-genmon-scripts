a3e4716c230b128015e2e57d20910923d4a33c2d
Sotirios Roussis <xtonousou@gmail.com>
2017-05-03 05:12:15 +0300

    Added some comments

--- cpu-panel.sh
+++ cpu-panel.sh
@@ -8,13 +8,15 @@ readonly DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Recommended size is 24x24 px
 readonly ICON="${DIR}/icons/cpu/chip.png"
 
+# Array of available logical CPUs
 declare -ra CPU_ARRAY=($(awk '/MHz/{print $4}' /proc/cpuinfo | cut -f1 -d"."))
+# Number of logical CPU
 readonly NUM_OF_CPUS="${#CPU_ARRAY[@]}"
 
 # Tooltip
 MORE_INFO="<tool>"
-MORE_INFO+="┌ $(grep "model name" /proc/cpuinfo | cut -f2 -d ":" | sed -n 1p | sed -e 's/^[ \t]*//')\n"
-STEP=0
+MORE_INFO+="┌ $(grep "model name" /proc/cpuinfo | cut -f2 -d ":" | sed -n 1p | sed -e 's/^[ \t]*//')\n" # CPU vendor, model, clock
+STEP=0 # to generate CPU numbers (eg. CPU0, CPU1 ...)
 for CPU in "${CPU_ARRAY[@]}"; do
   STDOUT=$(( STDOUT + CPU ))
   MORE_INFO+="├─ CPU ${STEP}: ${CPU} MHz\n"
@@ -22,7 +24,7 @@ for CPU in "${CPU_ARRAY[@]}"; do
 done
 MORE_INFO+="└─ Temperature: $(sensors | awk '/[Pp]ackage/{print $4}')"
 MORE_INFO+="</tool>"
-STDOUT=$(( STDOUT / NUM_OF_CPUS ))
+STDOUT=$(( STDOUT / NUM_OF_CPUS )) # calculate average clock speed
 STDOUT=$(awk '{$1 = $1 / 1024; printf "%.2f%s", $1, " GHz"}' <<< "${STDOUT}")
 
 # Panel
