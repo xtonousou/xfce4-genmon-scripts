716e447ef957ca46f81c0dddaeeffcc0f090c2f0
Sotirios Roussis <xtonousou@gmail.com>
2017-05-05 04:28:00 +0300

    Added datetime easter egg ^_^

--- datetime-panel.sh
+++ datetime-panel.sh
@@ -9,6 +9,8 @@ readonly DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Recommended size is 24x24 px
 if [ "$(date +%d%m)" -eq 2604 ]; then # birthday, 2604 stands for April 26
   readonly ICON="${DIR}/icons/datetime/cake-variant.png"
+elif [ "$(date +%H%M)" -eq 0420 ] || [ "$(date +%H%M)" -eq 1620 ]; then # heyy that's pretty good
+  readonly ICON="${DIR}/icons/datetime/weed.png"
 else
   readonly ICON="${DIR}/icons/datetime/clock.png"
 fi
