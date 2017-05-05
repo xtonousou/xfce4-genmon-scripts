c56550574cedc9e4cf9d85b5f43ee8f1e0c83711
Sotirios Roussis <xtonousou@gmail.com>
2017-05-06 00:07:09 +0300

    Added LC_ALL to ensure proper date comparison

--- datetime-panel.sh
+++ datetime-panel.sh
@@ -7,18 +7,18 @@ readonly DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Optional icon to display before the text
 # Insert the absolute path of the icon
 # Recommended size is 24x24 px
-if [ "$(date +%d%m)" -eq 2604 ]; then # birthday, 2604 stands for April 26
+if [ "$(LC_ALL=en_US.UTF-8 date +%d%m)" -eq 2604 ]; then # birthday, 2604 stands for April 26
   readonly ICON="${DIR}/icons/datetime/cake-variant.png"
-elif [ "$(date +%H%M)" -eq 0420 ] \
-  || [ "$(date +%H%M)" -eq 1620 ] \
-  || [ "$(date +%d%m)" -eq 2004 ]; then # heyy that's pretty good
+elif [ "$(LC_ALL=en_US.UTF-8 date +%H%M)" -eq 0420 ] \
+  || [ "$(LC_ALL=en_US.UTF-8 date +%H%M)" -eq 1620 ] \
+  || [ "$(LC_ALL=en_US.UTF-8 date +%d%m)" -eq 2004 ]; then # heyy that's pretty good
   readonly ICON="${DIR}/icons/datetime/weed.png"
 else
   readonly ICON="${DIR}/icons/datetime/clock.png"
 fi
 
-readonly TIME=$(date +%T)
-readonly DATE=$(date)
+readonly TIME=$(LC_ALL=en_US.UTF-8 date +%T)
+readonly DATE=$(LC_ALL=en_US.UTF-8 date)
 
 # Panel
 if [[ $(file -b "${ICON}") =~ PNG|SVG ]]; then
