a4af32871c8e23b8e5cdf077cc29eab04616e975
Sotirios Roussis <xtonousou@gmail.com>
2017-05-11 02:04:34 +0300

    Updated dependencies, removed extra va

--- eject.sh
+++ eject.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
-# Dependencies: coreutils, gksu, util-linux, zenity
+# Dependencies: coreutils, gawk, gksu, util-linux, zenity
 
-readonly TITLE="Select the source you want to unmount"
 declare -a DRIVES_ARRAY=($(df --sync --output=source | awk '/^\/dev\//{print $1}' | sort -V))
 declare -a FSTYPE_ARRAY=($(df --sync --output=source,fstype | sort -V | awk '/^\/dev\//{print $2}'))
 declare -a MNTPOINT_ARRAY=($(df --sync --output=source,target | sort -V | awk '/^\/dev\//{print $2}'))
 declare -a TO_BE_MOUNTED_ARRAY
 
 # zenity GUI
-readonly CHOICES=$(zenity --list --text "${TITLE}" --width 512 --height 256 \
+readonly CHOICES=$(zenity --list --width 512 --height 256 \
   --checklist --column "Unmount" --column "Source" --column "Filesystem" \
   --column "Mountpoint" \
   $(for ITEM in "${!DRIVES_ARRAY[@]}"; do \
