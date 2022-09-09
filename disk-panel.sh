4ddf2c0d95045d7750bf19c2f36837d52d8930e6
xtonousou <root@xtonousou.com>
2022-09-09 20:45:26 +0300

    Refactored disk panel and removed some unused files in project

--- disk-panel.sh
+++ disk-panel.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Dependencies: bash>=3.2, bleachbit(optional), coreutils, file, gawk)
+# Dependencies: bash>=3.2, bleachbit(optional), coreutils, file, gawk, hddtemp, bc
 
 # Makes the script more portable
 readonly DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -11,57 +11,47 @@ readonly ICON="${DIR}/icons/disk/database.png"
 
 # Command to grab current HDD's temperature
 # Change --unit to F if you don't want Celsius as default
+# Virtual Machines will report 0 ℃
 GET_TEMP=$(sudo hddtemp --unit=C --wake-up --quiet --numeric /dev/sda)
 
 # To determine if colors are applied
 OVERHEAT=0
 
 # Panel
+INFO=""
 if hash bleachbit &> /dev/null; then
-  INFO="<click>bleachbit &> /dev/null</click>"
+  INFO+="<click>bleachbit &> /dev/null</click>"
+elif hash gparted &> /dev/null; then
+  INFO+="<click>gparted &> /dev/null</click>"
 fi
 
 if [[ $(file -b "${ICON}") =~ PNG|SVG ]]; then
   INFO+="<img>${ICON}</img>"
-  INFO+="<txt>"
-else
-  INFO+="<txt>"
 fi
 
-[[ "${GET_TEMP}" -gt 42 ]] && \
-  OVERHEAT=1 && \
-    INFO+="<span weight='Bold' fgcolor='#FF5D5D'>"
+USED_SPACE=$(awk '{$1 = $1 / 1048576; printf "%.2f", $1}' <<< $(df / | awk '/\/dev/{print $3}'))
+TOTAL_SPACE=$(awk '{$1 = $1 / 1048576; printf "%.2f", $1}' <<< $(df / | awk '/\/dev/{print $2}'))
+PERCENTAGE=$(echo $(echo "${USED_SPACE} * 100" | bc -l) / "${TOTAL_SPACE}" | bc -l)
 
-INFO+="$(awk '{$1 = $1 / 1048576; printf "%.2f", $1}' <<< $(df / | awk '/\/dev/{print $3}'))"
-INFO+="／"
-INFO+="$(awk '{$1 = $1 / 1048576; printf "%.2f", $1}' <<< $(df / | awk '/\/dev/{print $2}'))"
-INFO+=" GB"
-
-# Close span tag if warning colors are applied
-[[ "${OVERHEAT}" -eq 1 ]] && \
-  INFO+="</span>"
-
-INFO+="</txt>"
+# Percentage bar
+BAR="<bar>${PERCENTAGE}</bar>"
 
 # Tooltip
 MORE_INFO="<tool>"
-MORE_INFO+="┌ $(df -h / | awk '/\/dev/{print $1}' | head -n1)\n"
-MORE_INFO+="├─ Vendor:\t\t$(awk '/[V]endor:/{print $2}' /proc/scsi/scsi)\n"
-MORE_INFO+="├─ Model:\t\t$(awk '/[Mm]odel:/{print $4, $5}' /proc/scsi/scsi)\n"
-MORE_INFO+="├─ Revision:\t\t$(awk '/[Rr]ev:/{print $7}' /proc/scsi/scsi)\n"
-MORE_INFO+="├─ Type:\t\t\t$(awk '/[Tt]ype:/{print $2, $3, $4}' /proc/scsi/scsi)\n"
-
+MORE_INFO+="Usage:\\t\\t${USED_SPACE} / ${TOTAL_SPACE} GB\\n"
 ############# !!!!!!!!!!!!!!!!!!! IMPORTANT !!!!!!!!!!!!!!! ####################
 # Append the following line to /etc/sudoers (Recommended: edit with visudo)
 # %wheel ALL=(ALL) NOPASSWD: /usr/bin/hddtemp
 # If you don't do the above, xfce4-panel will freeze and
-# eventually will freeze X too. So, be careful!
-MORE_INFO+="└─ Temperature:\t${GET_TEMP} ℃"
-
+# eventually will freeze X too and maybe PAM as well. So, be careful!
+MORE_INFO+="Temperature:\\t${GET_TEMP} ℃"
 MORE_INFO+="</tool>"
 
 # Panel Print
 echo -e "${INFO}"
+
+# Bar print
+echo -e "${BAR}"
 
 # Tooltip Print
 echo -e "${MORE_INFO}"
